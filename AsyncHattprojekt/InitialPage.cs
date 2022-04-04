534305c4356ea7347ca50716a9d92a6f4b15613e
scottjander <83708549+scottjander@users.noreply.github.com>
2022-04-04 13:08:07 +0200

    Initial Commit

---
+++ AsyncHattprojekt/InitialPage.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DataLayer;
+using DataLayer.Models;
+
+
+namespace AsyncHattprojekt
+{
+    public partial class InitialPage : Form
+    {
+        public InitialPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var context = new HatDbContext())
+            {
+                var nyanstalld = new Employees() {Name = textBox1.Text};
+                context.Employees.Add(nyanstalld);
+                context.SaveChanges();
+            }
+
+        }
+    }
+}
---
+++ DataLayer/HatDbContext.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Data.Entity;
+using System.Linq;
+using DataLayer.Models;
+
+
+namespace DataLayer
+{
+    public class HatDbContext : DbContext
+    {
+        public DbSet<Employees> Employees { get; set; }
+
+        // Your context has been configured to use a 'HatDbContext' connection string from your application's 
+        // configuration file (App.config or Web.config). By default, this connection string targets the 
+        // 'AsyncHattprojekt.HatDbContext' database on your LocalDb instance. 
+        // 
+        // If you wish to target a different database and/or database provider, modify the 'HatDbContext' 
+        // connection string in the application configuration file.
+        public HatDbContext()
+            : base("name=HatDbContext")
+        {
+        }
+
+        // Add a DbSet for each entity type that you want to include in your model. For more information 
+        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        // public virtual DbSet<MyEntity> MyEntities { get; set; } test
+    }
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
