dc22b8a620aa0aca8c4bcb789afed39d840c3692
Max Canderyd <Max.canderyd@gmail.com>
2022-04-06 11:52:45 +0200

    Lite repository

---
+++ BusinessLayer/Controllers/AnvandareController.cs
@@ -0,0 +1,21 @@
+﻿using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class AnvandareController
+    {
+        public AnvandareRepository AnvandareRepository
+        {
+            get { return new AnvandareRepository(); }
+        }
+
+
+
+
+    }
+}
---
+++ DataLayer/Repository/AnvandareRepository.cs
@@ -0,0 +1,24 @@
+﻿using DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace DataLayer
+{
+    public class AnvandareRepository
+    {
+
+        public List<string> GetAllNames()
+        {
+           using (var context = new HatDbContext())
+            {
+               
+            }
+        }
+
+
+    }
+}
