cb8c0b532993ad82877f88e997b7811413645991
robinaziz <rabbebabbe@hotmail.com>
2022-04-13 12:36:10 +0200

    frakrt

--- AsyncHattprojekt/FraktsedelForm.Designer.cs
+++ AsyncHattprojekt/FraktsedelForm.Designer.cs
@@ -28,19 +28,86 @@ namespace AsyncHattprojekt
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblStreet = new System.Windows.Forms.Label();
+            this.lblTown = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(50, 81);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(77, 29);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Namn";
+            // 
+            // lblStreet
+            // 
+            this.lblStreet.AutoSize = true;
+            this.lblStreet.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStreet.Location = new System.Drawing.Point(50, 151);
+            this.lblStreet.Name = "lblStreet";
+            this.lblStreet.Size = new System.Drawing.Size(59, 29);
+            this.lblStreet.TabIndex = 1;
+            this.lblStreet.Text = "gata";
+            // 
+            // lblTown
+            // 
+            this.lblTown.AutoSize = true;
+            this.lblTown.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTown.Location = new System.Drawing.Point(50, 220);
+            this.lblTown.Name = "lblTown";
+            this.lblTown.Size = new System.Drawing.Size(62, 29);
+            this.lblTown.TabIndex = 2;
+            this.lblTown.Text = "Stad";
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDate.Location = new System.Drawing.Point(608, 366);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(82, 29);
+            this.lblDate.TabIndex = 3;
+            this.lblDate.Text = "Datum";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(28, 398);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(774, 179);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
             // 
             // FraktsedelForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(870, 747);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.lblTown);
+            this.Controls.Add(this.lblStreet);
+            this.Controls.Add(this.lblName);
             this.Name = "FraktsedelForm";
             this.Text = "FraktsedelForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblStreet;
+        private System.Windows.Forms.Label lblTown;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
