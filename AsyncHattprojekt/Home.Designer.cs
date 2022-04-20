7b6666f9629997c51096c9fc0113e86ef203351d
LFEriksson <96394936+LFEriksson@users.noreply.github.com>
2022-04-20 13:39:24 +0200

    Nytt UI

---
+++ AsyncHattprojekt/Home.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace AsyncHattprojekt
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHome = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblHome
+            // 
+            this.lblHome.AutoSize = true;
+            this.lblHome.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHome.Location = new System.Drawing.Point(649, 471);
+            this.lblHome.Name = "lblHome";
+            this.lblHome.Size = new System.Drawing.Size(108, 39);
+            this.lblHome.TabIndex = 0;
+            this.lblHome.Text = "Home";
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1452, 1078);
+            this.Controls.Add(this.lblHome);
+            this.Name = "Home";
+            this.Text = "Home";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHome;
+    }
+}
---
+++ AsyncHattprojekt/NewUI.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncHattprojekt
+{
+    public partial class NewUI : Form
+    {
+        private Form activeForm = null;
+        public NewUI()
+        {
+            InitializeComponent();
+            customizeDesign();
+            new CustomerRegisterForm();
+        }
+
+        private void customizeDesign()
+        {
+            panelPurchaseSubmenu.Visible = false;
+        }
+
+        private void hideSubMenu()
+        {
+            if (panelPurchaseSubmenu.Visible == true)
+                panelPurchaseSubmenu.Visible = false;
+        }
+
+        private void showSubMenu(Panel subMenu)
+        {
+            if (subMenu.Visible == false) {
+                hideSubMenu();
+                subMenu.Visible = true;
+            }
+            else
+                subMenu.Visible = false;
+        }
+
+        private void setMenuColor(Color color)
+        {
+            PanelBoarder.BackColor = color;
+            panelOrder.BackColor = color;
+            panelMenu.BackColor = color;
+        }
+
+        public void OpenChildForm(Form childForm)
+        {
+            if ( activeForm != null )
+                activeForm.Close();
+            activeForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            panelChildForm.Controls.Add(childForm);
+            panelChildForm.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+        }
+
+
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Home());
+            setMenuColor(btnHome.BackColor);
+        }
+
+        private void btnPurchase_Click(object sender, EventArgs e)
+        {
+            showSubMenu(panelPurchaseSubmenu);
+        }
+
+        private void btnDHat_Click(object sender, EventArgs e)
+        {
+            hideSubMenu();
+            setMenuColor(btnPurchase.BackColor);
+        }
+
+        private void btnSHat_Click(object sender, EventArgs e)
+        {
+            hideSubMenu();
+            setMenuColor(btnPurchase.BackColor);
+        }
+
+        private void btnOrder_Click(object sender, EventArgs e)
+        {
+            setMenuColor(btnOrder.BackColor);
+        }
+
+        private void btnStock_Click(object sender, EventArgs e)
+        {
+            hideSubMenu();
+            setMenuColor(btnStock.BackColor);
+        }
+
+        private void btnCustomer_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new CustomerRegisterForm());
+            setMenuColor(btnCustomer.BackColor);
+        }
+
+        private void btnHistory_Click(object sender, EventArgs e)
+        {
+            hideSubMenu();
+            setMenuColor(btnEconomy.BackColor);
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NewUI_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
