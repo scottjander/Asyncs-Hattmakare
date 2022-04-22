532a260b6fef8df833136696f9ce806d1f55bd60
erikhawerman <84011896+erikhawerman@users.noreply.github.com>
2022-04-22 12:41:57 +0200

    changed startpage

--- AsyncHattprojekt/Program.cs
+++ AsyncHattprojekt/Program.cs
@@ -16,7 +16,7 @@ namespace AsyncHattprojekt
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ManageHatts());
+            Application.Run(new InitialPage());
         }
     }
 }
